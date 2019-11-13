330d5772cdd8227605cafe05872a43ae27f9523f
Saeid Ostad <SOstad@kajimausa.com>
2019-11-13 14:11:29 -0500

    added more ts typedef

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,5 +1,9 @@
 import { app, BrowserWindow } from "electron";
 
+declare global {
+  const MAIN_WINDOW_WEBPACK_ENTRY: string;
+}
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   // eslint-disable-line global-require
@@ -21,7 +25,6 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  // @ts-ignore
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
